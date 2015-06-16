a925387decaf93533c27f66cce2e887688ed5316
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2015-06-16 17:25:22 +0100

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,25 @@
+module ApplicationHelper
+
+  def today?(year, month, day)
+    today = DateTime.now
+    current_year = today.year
+    current_month = today.month
+    current_day = today.day
+    current_year == year && month == current_month && current_day == day
+  end
+
+  def month_name(date)
+    I18n.l(date, format: "%B")
+  end
+
+  def month_number(date)
+    date.strftime("%m").to_i
+  end
+
+
+  def first_day(date)
+    week_days = %w(SEG TER QUA QUI SEX SAB DOM)
+    week_days[date.wday - 1]
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'application#home'
+
+
+  # resources :users
+
+end
