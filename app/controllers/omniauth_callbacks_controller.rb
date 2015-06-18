1fe1606e32041f27a8583bf99546d6b8ca67b0ce
junhanamaki <jun.hanamaki@gmail.com>
2015-06-18 18:23:43 +0100

    remove prys

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     report = OmniauthCallbacksApi::GoogleOauth2.run(omniauth: request.env["omniauth.auth"])
-binding.pry
+
     if report.success?
       sign_in_and_redirect report.data.user
     else
     end
-  end
-
-  def failure
-    binding.pry
   end
 end
