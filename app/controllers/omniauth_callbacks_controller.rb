3ceae207d56ffa94ce77fd295baf1d3444666a7b
junhanamaki <jun.hanamaki@gmail.com>
2015-06-18 18:22:34 +0100

    using flow :D

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    binding.pry
     report = OmniauthCallbacksApi::GoogleOauth2.run(omniauth: request.env["omniauth.auth"])
-
+binding.pry
     if report.success?
       sign_in_and_redirect report.data.user
     else
--- app/processes/users/from_omniauth.rb
+++ app/processes/users/from_omniauth.rb
@@ -2,18 +2,18 @@ module Users
   class FromOmniauth
     include WFlow::Process
 
-    data_reader :omniauth_auth
+    data_reader :omniauth
     data_writer :user
 
     def setup
-      flow.failure! if omniauth_auth.nil?
+      flow.failure! if omniauth.nil?
     end
 
     def perform
-      self.user = where(provider: auth.provider, uid: auth.uid).first_or_create do |u|
-        u.provider = auth.provider
-        u.uid      = auth.uid
-        u.email    = auth.info.email
+      self.user = User.where(provider: omniauth.provider, uid: omniauth.uid).first_or_create do |u|
+        u.provider = omniauth.provider
+        u.uid      = omniauth.uid
+        u.email    = omniauth.info.email
         u.password = Devise.friendly_token[0,20]
       end
     end
