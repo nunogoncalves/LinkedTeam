29dd9d5a6e564acb0d71c04a2ac0951ef4cadc85
junhanamaki <jun.hanamaki@gmail.com>
2015-06-21 22:35:41 +0100

    members and managers

--- app/models/team.rb
+++ app/models/team.rb
@@ -14,10 +14,11 @@ class Team < ActiveRecord::Base
   scope :managed_by, ->(user) { includes(:managers).where(users: { id: user.id }) }
 
   def is_member?(user)
-
+    members.include?(user)
   end
 
   def is_manager?(user)
+    managers.include?(user)
   end
 
 end
