3e6e6d33fc7fc534bec78e1656f9f606d68ecde9
junhanamaki <jun.hanamaki@gmail.com>
2015-06-23 22:03:59 +0100

    fury before the storm!

--- app/models/calendars/user.rb
+++ app/models/calendars/user.rb
@@ -2,6 +2,10 @@ class Calendars::User < ActiveRecord::Base
 
   belongs_to :user, class: ::User
 
+  has_many :calendars_vacations,
+           class:       Calendars::Vacation,
+           foreign_key: :calendars_user_id
+
   has_many :calendars_annual_leaves,
            class:       Calendars::AnnualLeave,
            foreign_key: :calendars_user_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623201556) do
+ActiveRecord::Schema.define(version: 20150623204743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 20150623201556) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "calendars_vacations", force: :cascade do |t|
+    t.integer  "calendars_user_id"
+    t.string   "state"
+    t.integer  "year"
+    t.integer  "month"
+    t.integer  "day"
+    t.date     "date"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "team_managers", force: :cascade do |t|
